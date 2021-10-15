626b30e809f17fa88a06d603bb8e215c89f1c99d
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-10-15 10:52:36 -0300

    chore: splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { StackRoutes } from './stack.routes'
 
 
-
 export function Routes(){
    return (
       <NavigationContainer>
